96e8153b17f33c1ae32df3f7731326cdd480b041
PabloDelgadoSoto <pablodelsot@gmail.com>
2025-01-10 20:53:32 +0100

    parte servicio

---
+++ Ejercicio2Servicios/Ejercicio2Servicios/WebService1.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Ejercicio2Servicios
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public int Caracteres(string str)
+        {
+            return str.Length;
+        }
+
+        [WebMethod]
+        public int Potencia(int n1, int n2)
+        {
+            int resultado = 1;
+            for(int i = 0; i < n2;i++)
+            {
+                resultado *= n1;
+            }
+            return resultado;
+        }
+    }
+}
